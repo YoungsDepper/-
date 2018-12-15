7a190dfcf6b61d291fef7d37642e56c64e847d4a
1601842912@qq.com <youngs>
2018-12-15 15:50:00 +0800

    登陆界面静态完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -3,7 +3,7 @@
  */
 
 
-$(function() {
+$(function () {
 
   /*
    * 1. 进行表单校验配置
@@ -12,50 +12,44 @@ $(function() {
    *        (2) 密码不能为空, 长度为6-12位
    * */
   $('#form').bootstrapValidator({
-
-    // 配置图标
     feedbackIcons: {
-      valid: 'glyphicon glyphicon-ok',         // 校验成功
-      invalid: 'glyphicon glyphicon-remove',   // 校验失败
-      validating: 'glyphicon glyphicon-refresh'  // 校验中
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
     },
-
-    // 配置校验字段    需要先给input框配置 name
     fields: {
+      //校验用户名，对应name表单的name属性
       username: {
-        // 配置校验规则
         validators: {
-          // 配置一个非空
+          //不能为空
           notEmpty: {
-            message: "用户名不能为空"
+            message: '用户名不能为空'
           },
-          // 长度校验
+          //长度校验
           stringLength: {
             min: 2,
             max: 6,
-            message: "用户名长度必须是2-6位"
-          }
+            message: '用户名长度必须在6到30之间'
+          },
+          //正则校验
+          // regexp: {
+          //   regexp: /^[a-zA-Z0-9_\.]+$/,
+          //   message: '用户名由数字字母下划线和.组成'
+          // }
         }
       },
-
       password: {
-        // 配置校验规则
         validators: {
-          // 非空校验
           notEmpty: {
-            message: "密码不能为空"
+            message: '用户名不能为空'
           },
-          // 长度校验
-          stringLength:{
+          stringLength: {
             min: 6,
             max: 12,
-            message: "密码长度必须是6-12位"
-          }
+            message: '用户名长度必须在6到30之间'
+          },
         }
       }
     }
-
   });
-
-
 })
